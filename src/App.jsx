2ca9c3ac5ba1d68df61126ff29c6ac67bde36d90
Zakaria Arshad <za2196@nyu.edu>
2024-11-03 22:21:45 -0500

    initial footer design

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,13 @@
+import Footer from './components/footer/Footer'
+
 const App = () => {
   return (
-    <h1 className="text-3xl font-bold underline font-inter text-blue">
-      Hello world!
-    </h1>
+    <>
+      <h1 className="text-3xl font-bold underline font-inter text-blue">
+        Hello world!
+      </h1>
+      <Footer />
+    </>
   )
 }
 
