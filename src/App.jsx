482f38129c359e233f6d4e0b046d0290e3ce54bd
Riley Dou <94800352+rilieo@users.noreply.github.com>
2024-12-04 21:12:36 -0500

    Create about section (#7)
    create about section

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
+import About from './sections/about/About';
 import Footer from './components/footer/Footer'
 import Navbar from './components/navbar/Navbar'
 import { useOpen } from './context/OpenContext';
-import About from './sections/about/About';
 
 const App = () => {
   const { isOpen } = useOpen();
@@ -9,6 +9,7 @@ const App = () => {
   return (
     <div className="h-screen box-border m-0 p-0">
       <Navbar />
+      <About />
       { !isOpen && <Footer /> }
     </div>
   )
