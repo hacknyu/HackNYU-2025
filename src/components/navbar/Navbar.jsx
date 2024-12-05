66eee707eaa3f10d0ce77b5091ccf89fc679847f
rilieo <rilesdou@gmail.com>
2024-12-04 23:15:29 -0500

    add smooth scrolling

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,8 +1,8 @@
 import React, { useState, useEffect } from 'react';
-import { useOpen } from '../../context/OpenContext';
+import { Link } from 'react-scroll';
 
 const Navbar = () => {
-  const { isOpen, setIsOpen } = useOpen();
+  const [isOpen, setIsOpen] = useState(false);
 
   useEffect(() => {
     const circleElement = document.querySelector('.circle');
@@ -21,18 +21,46 @@ const Navbar = () => {
 
   return (
     <nav
-      className={`fixed flex text-xl font-bold items-center overflow-hidden w-full ${
+      className={`fixed flex text-xl items-center overflow-hidden w-full bg-white z-10 ${
         isOpen ? 'h-full' : 'h-[75px] justify-between'
       }`}
     >
       <div className={`w-[100px] ml-5 ${isOpen ? 'hidden' : 'block'}`}>
         <img src="/hacklogo.svg" alt="HackNYU logo"></img>
       </div>
-      <div className="hidden md:flex md:space-x-10 md:text-blue md:text-2xl">
-        <a href="#about">About</a>
-        <a href="#tracks">Tracks</a>
-        <a href="#faq">FAQ</a>
-        <a href="#team">Team</a>
+      <div className="hidden font-bold md:flex md:space-x-10 md:text-blue md:text-2xl">
+      <Link
+          to="about"
+          spy={true}
+          smooth={true}
+          duration={500}
+          className="hover:cursor-pointer"
+          onClick={handleClick}
+        >
+          About
+        </Link>
+
+        <Link
+          to="tracks"
+          spy={true}
+          smooth={true}
+          duration={500}
+          className="hover:cursor-pointer"
+          onClick={handleClick}
+        >
+          Tracks
+        </Link>
+
+        <Link
+          to="faq"
+          spy={true}
+          smooth={true}
+          duration={500}
+          className="hover:cursor-pointer"
+          onClick={handleClick}
+        >
+          FAQ
+        </Link>
       </div>
       <div className="relative flex flex-col justify-center items-center w-full h-full md:hidden ">
         <div className="circle"></div>
@@ -43,22 +71,54 @@ const Navbar = () => {
           </span>
         :
         <>
-          <div className="absolute top-5 right-2 w-[40px] h-[40px] hover:cursor-pointer text-white">
+          <div className="absolute top-5 right-2 w-[40px] h-[40px] hover:cursor-pointer text-white font-bold">
             <span className="text-4xl" onClick={handleClick}>x</span>
           </div>
-          <div className="flex flex-col space-y-10 text-white text-2xl text-center">
-            <a href="#about" onClick={handleClick}>About</a>
-            <a href="#tracks" onClick={handleClick}>Tracks</a>
-            <a href="#faq" onClick={handleClick}>FAQ</a>
-            <a href="#team" onClick={handleClick}>Team</a>
+          <div className="flex flex-col space-y-10 text-white text-2xl text-center font-bold">
+            <Link
+              to="about"
+              spy={true}
+              smooth={true}
+              duration={500}
+              className="hover:cursor-pointer"
+              onClick={handleClick}
+            >
+              About
+            </Link>
+
+            <Link
+              to="tracks"
+              spy={true}
+              smooth={true}
+              duration={500}
+              className="hover:cursor-pointer"
+              onClick={handleClick}
+            >
+              Tracks
+            </Link>
+
+            <Link
+              to="faq"
+              spy={true}
+              smooth={true}
+              duration={500}
+              className="hover:cursor-pointer"
+              onClick={handleClick}
+            >
+              FAQ
+            </Link>
           </div>
-        </>
+          </>
         } 
       </div>
       <div className="md:w-[100px] md:mr-5">
-        <button className="hidden md:block bg-blue text-white p-2 md:text-xl border-none rounded cursor-pointer transition-colors duration-300 hover:bg-orange-600">
+        <a className="hidden md:block bg-blue text-white py-3 px-4 md:text-xl border-none rounded cursor-pointer transition-colors duration-300 hover:bg-blue-500"
+            href="https://forms.gle/eUhikbS4VpcnXTvA9"
+            target="_blank"
+            rel="noopener noreferrer"
+        >
           Register
-        </button>
+        </a>
       </div>
     </nav>
   );
