9c20ab4f10927d1c2ceba236d9f43a627b9e98c3
Sarthak Goel <sarthakgoel_97@hotmail.com>
2024-12-12 01:16:56 -0500

    feat: add MLH event badge to the website

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -136,6 +136,8 @@ const Navbar = () => {
           Register
         </a>
       </div>
+
+      <a id="mlh-trust-badge" className='mlh-badge' href="https://mlh.io/na?utm_source=na-hackathon&utm_medium=TrustBadge&utm_campaign=2025-season&utm_content=white" target="_blank"><img src="https://s3.amazonaws.com/logged-assets/trust-badge/2025/mlh-trust-badge-2025-white.svg" alt="Major League Hacking 2025 Hackathon Season" className='width-100'/></a>
     </nav>
   );
 };
