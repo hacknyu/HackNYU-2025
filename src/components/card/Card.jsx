9fd5ce94e9233fa3a827fa0afe29b0f0f21153af
Sarthak Goel <sarthakgoel_97@hotmail.com>
2025-01-06 22:32:20 -0500

    Move Flip text to same line as icon and update on hover properties

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -13,20 +13,20 @@ const Card = ({ info }) => {
 
     return (
         <ReactCardFlip isFlipped={flipped} flipDirection="horizontal">
-        <div onClick={handleClick} className={cardStyle}>
-            <div className="text-center">
-            <h2 className="text-2xl">{info.header}</h2>
-            <h3 className="text-2xl">{info.subheader}</h3>
+            <div onClick={handleClick} className={`${cardStyle} group`}>
+                <div className="text-center">
+                    <h2 className="text-2xl">{info.header}</h2>
+                    <h3 className="text-2xl">{info.subheader}</h3>
+                </div>
+                <div className="mt-10 flex flex-row items-center space-x-2 group-hover:scale-105 group-hover:text-orange-600 transition-transform duration-300 ease-in-out">
+                    <span className="text-xl">Flip</span>
+                    <img src={flipArrow} alt="flip arrow" className="w-10 h-10 mb-2" />
+                </div>
             </div>
-            <div className="mt-10 flex flex-col items-center">
-                <img src={flipArrow} alt="flip arrow" className="w-10 h-10 mb-2" />
-                <span className="text-md font-medium text-white">Flip</span>
-            </div>
-        </div>
 
-        <div onClick={handleClick} className={cardStyle}>
-            <p className="text-md md:text-lg text-center">{info.description}</p>
-        </div>
+            <div onClick={handleClick} className={cardStyle}>
+                <p className="text-md md:text-lg text-center">{info.description}</p>
+            </div>
         </ReactCardFlip>
     )
 }
