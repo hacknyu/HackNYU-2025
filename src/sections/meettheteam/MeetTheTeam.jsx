7b381571d430c59c906e7f8cb03fdae73356c366
Fahim H <fahimhussain21@gmail.com>
2025-02-01 17:28:08 -0500

    adjust ordering and section spacing

--- src/sections/meettheteam/MeetTheTeam.jsx
+++ src/sections/meettheteam/MeetTheTeam.jsx
@@ -28,16 +28,92 @@ const teamMembers = [
     image: shreyImg,
   },
   {
+    id: 16,
+    name: "Rishi Rana",
+    title: "Vice President",
+    image: rishiImg,
+  },
+  {
     id: 8,
     name: "Cassandra Salazar",
     title: "Vice President",
     image: cassandraImg,
   },
+  // Bam
   {
-    id: 16,
-    name: "Rishi Rana",
-    title: "Vice President",
-    image: rishiImg,
+    id: 13,
+    name: "Ana Eremina",
+    title: "Design PM",
+    image: anaImg,
+  },
+  {
+    id: 12,
+    name: "Vandana Rajesh",
+    title: "Design",
+    image: vandanaImg,
+  },
+  // Cher
+  {
+    id: 15,
+    name: "Heloise Wu",
+    title: "Design",
+    image: heloiseImg,
+  },
+  {
+    id: 6,
+    name: "Tanzina Sumona",
+    title: "Logistics PM",
+    image: tanzinaImg,
+  },
+  // Ashley
+  {
+    id: 11,
+    name: "Aalia Imran",
+    title: "Logistics",
+    image: aaliaImg,
+  },
+  // Daniel
+  {
+    id: 3,
+    name: "Kayna Huang",
+    title: "Media and Marketing",
+    image: kaynaImg,
+  },
+  {
+    id: 14,
+    name: "Hailey Kim",
+    title: "Media and Marketing",
+    image: haileyImg,
+  },
+  {
+    id: 2,
+    name: "Isaac Zhang",
+    title: "Media and Marketing",
+    image: isaacImg,
+  },
+  {
+    id: 7,
+    name: "Helen Zhou",
+    title: "Media and Marketing",
+    image: helenImg,
+  },
+  {
+    id: 9,
+    name: "Riley Dou",
+    title: "Tech Development PM",
+    image: rileyImg,
+  },
+  {
+    id: 99,
+    name: "Zakaria Arshad",
+    title: "Tech Development",
+    image: zakImg,
+  },
+  {
+    id: 10,
+    name: "Sarthak Goel",
+    title: "Tech Development",
+    image: sarthakImg,
   },
   {
     id: 1,
@@ -51,83 +127,11 @@ const teamMembers = [
     title: "Tech Development",
     image: mohanImg,
   },
-  {
-    id: 9,
-    name: "Riley Dou",
-    title: "Tech Development",
-    image: rileyImg,
-  },
-  {
-    id: 10,
-    name: "Sarthak Goel",
-    title: "Tech Development",
-    image: sarthakImg,
-  },
-  {
-    id: 99,
-    name: "Zakaria Arshad",
-    title: "Tech Development",
-    image: zakImg,
-  },
-  {
-    id: 6,
-    name: "Tanzina Sumona",
-    title: "Logistics",
-    image: tanzinaImg,
-  },
-  {
-    id: 11,
-    name: "Aalia Imran",
-    title: "Logistics",
-    image: aaliaImg,
-  },
-  {
-    id: 12,
-    name: "Vandana Rajesh",
-    title: "Design",
-    image: vandanaImg,
-  },
-  {
-    id: 13,
-    name: "Ana Eremina",
-    title: "Design",
-    image: anaImg,
-  },
-  {
-    id: 15,
-    name: "Heloise Wu",
-    title: "Design",
-    image: heloiseImg,
-  },
-  {
-    id: 2,
-    name: "Isaac Zhang",
-    title: "Media and Marketing",
-    image: isaacImg,
-  },
-  {
-    id: 3,
-    name: "Kayna Huang",
-    title: "Media and Marketing",
-    image: kaynaImg,
-  },
-  {
-    id: 7,
-    name: "Helen Zhou",
-    title: "Media and Marketing",
-    image: helenImg,
-  },
-  {
-    id: 14,
-    name: "Hailey Kim",
-    title: "Media and Marketing",
-    image: haileyImg,
-  },
 ];
 
 export default function MeetTheTeam() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen p-4">
+    <div className="flex flex-col items-center justify-center min-h-screen p-8 md:my-20">
       <h2 className="text-blue text-3xl font-bold mb-8">Meet the Team</h2>
       <div className="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
         {teamMembers.map((member) => (
