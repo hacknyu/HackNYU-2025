216c4cd036202815b854e5ff5a8415196bc13095
Fahim H <fahimhussain21@gmail.com>
2025-02-02 13:50:06 -0500

    add team section to navbar

--- src/sections/meettheteam/MeetTheTeam.jsx
+++ src/sections/meettheteam/MeetTheTeam.jsx
@@ -131,7 +131,7 @@ const teamMembers = [
 
 export default function MeetTheTeam() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen p-8 md:my-20">
+    <div id="team" className="flex flex-col items-center justify-center min-h-screen p-8 md:my-20">
       <h2 className="text-blue text-3xl font-bold mb-8">Meet the Team</h2>
       <div className="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
         {teamMembers.map((member) => (
